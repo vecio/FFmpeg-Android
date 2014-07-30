293e1518bec3c8dc0ce6c0c2719e4ee65ad956a5
Xavier Hallade <xavier.hallade@intel.com>
2014-07-30 19:37:48 +0200

    added x86 target

--- FFmpeg-Android.sh
+++ FFmpeg-Android.sh
@@ -27,9 +27,10 @@
 # 0. Install git and Android ndk
 # 1. $ export ANDROID_NDK=/path/to/your/android-ndk
 # 2. $ ./FFmpeg-Android.sh
-# 3. libffmpeg.so will be built to build/ffmpeg/{neon,armv7,vfp,armv6}/
+# 3. libffmpeg.so will be built to build/ffmpeg/{neon,armv7,vfp,armv6,x86}/
 #
 #
+
 
 DEST=`pwd`/build/ffmpeg && rm -rf $DEST
 SOURCE=`pwd`/ffmpeg
@@ -49,27 +50,20 @@ patch -p1 <../FFmpeg-VPlayer.patch
 
 git log --pretty=format:%H -1 > ../ffmpeg-version
 
-TOOLCHAIN=/tmp/vplayer
-SYSROOT=$TOOLCHAIN/sysroot/
-$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-14 --install-dir=$TOOLCHAIN
+TOOLCHAIN_PREFIX=/tmp/vplayer
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-14 --install-dir=$TOOLCHAIN_PREFIX-arm --arch=arm
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-14 --install-dir=$TOOLCHAIN_PREFIX-x86 --arch=x86 --toolchain=x86-4.8 #using 4.8 instead of default 4.6 because of http://www.ffmpeg.org/faq.html#error_003a-can_0027t-find-a-register-in-class-_0027GENERAL_005fREGS_0027-while-reloading-_0027asm_0027 
 
-export PATH=$TOOLCHAIN/bin:$PATH
-export CC="ccache arm-linux-androideabi-gcc"
-export LD=arm-linux-androideabi-ld
-export AR=arm-linux-androideabi-ar
+export PATH=$TOOLCHAIN_PREFIX-arm/bin:$TOOLCHAIN_PREFIX-x86/bin:$PATH
 
-CFLAGS="-O3 -Wall -mthumb -pipe -fpic -fasm \
+CFLAGS="-O3 -Wall -pipe -fpic -fasm \
   -finline-limit=300 -ffast-math \
-  -fstrict-aliasing -Werror=strict-aliasing \
   -fmodulo-sched -fmodulo-sched-allow-regmoves \
   -Wno-psabi -Wa,--noexecstack \
-  -D__ARM_ARCH_5__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5TE__ \
   -DANDROID -DNDEBUG"
 
 FFMPEG_FLAGS="--target-os=linux \
-  --arch=arm \
   --enable-cross-compile \
-  --cross-prefix=arm-linux-androideabi- \
   --enable-shared \
   --disable-symver \
   --disable-doc \
@@ -96,26 +90,36 @@ FFMPEG_FLAGS="--target-os=linux \
   --enable-version3"
 
 
-for version in neon armv7 vfp armv6; do
+for version in neon armv7 vfp armv6 x86; do
 
   cd $SOURCE
 
   case $version in
     neon)
+      TARGET_ARCH=arm
       EXTRA_CFLAGS="-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mvectorize-with-neon-quad"
       EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
       ;;
     armv7)
+      TARGET_ARCH=arm
       EXTRA_CFLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
       EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
       ;;
     vfp)
+      TARGET_ARCH=arm
       EXTRA_CFLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=softfp"
       EXTRA_LDFLAGS=""
       ;;
     armv6)
+      TARGET_ARCH=arm
       EXTRA_CFLAGS="-march=armv6"
       EXTRA_LDFLAGS=""
+      ;;
+    x86)
+      TARGET_ARCH=x86
+      EXTRA_CFLAGS="-mtune=atom -mssse3 -mfpmath=sse"
+      EXTRA_LDFLAGS=""
+      EXTRA_FFMPEG_FLAGS="--disable-avx"
       ;;
     *)
       EXTRA_CFLAGS=""
@@ -123,8 +127,23 @@ for version in neon armv7 vfp armv6; do
       ;;
   esac
 
+  case $TARGET_ARCH in
+    arm)
+      CROSS_PREFIX=arm-linux-androideabi
+      EXTRA_CFLAGS="$EXTRA_CFLAGS -fstrict-aliasing -Werror=strict-aliasing -mthumb -D__ARM_ARCH_5__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5TE__"
+    ;;
+    x86)
+      CROSS_PREFIX=i686-linux-android
+      EXTRA_OBJS="libswscale/x86/*.o libswresample/x86/*.o"
+    ;;
+  esac
+
   PREFIX="$DEST/$version" && mkdir -p $PREFIX
-  FFMPEG_FLAGS="$FFMPEG_FLAGS --prefix=$PREFIX"
+  FFMPEG_FLAGS="$FFMPEG_FLAGS --arch=$TARGET_ARCH --cross-prefix=$CROSS_PREFIX- --prefix=$PREFIX $EXTRA_FFMPEG_FLAGS"
+
+  export CC="ccache ${CROSS_PREFIX}-gcc"
+  export LD=$CROSS_PREFIX-ld
+  export AR=$CROSS_PREFIX-ar
 
   ./configure $FFMPEG_FLAGS --extra-cflags="$CFLAGS $EXTRA_CFLAGS" --extra-ldflags="$EXTRA_LDFLAGS" | tee $PREFIX/configuration.txt
   cp config.* $PREFIX
@@ -135,9 +154,9 @@ for version in neon armv7 vfp armv6; do
   make install || exit 1
 
   rm libavcodec/inverse.o
-  $CC -lm -lz -shared --sysroot=$SYSROOT -Wl,--no-undefined -Wl,-z,noexecstack $EXTRA_LDFLAGS libavutil/*.o libavutil/arm/*.o libavcodec/*.o libavcodec/arm/*.o libavformat/*.o libswresample/*.o libswscale/*.o -o $PREFIX/libffmpeg.so
+  $CC -lm -lz -shared --sysroot=$TOOLCHAIN_PREFIX-$TARGET_ARCH/sysroot/ -Wl,--no-undefined -Wl,-z,noexecstack $EXTRA_LDFLAGS libavutil/*.o libavutil/$TARGET_ARCH/*.o libavcodec/*.o libavcodec/$TARGET_ARCH/*.o libavformat/*.o libswresample/*.o libswscale/*.o $EXTRA_OBJS -o $PREFIX/libffmpeg.so
 
   cp $PREFIX/libffmpeg.so $PREFIX/libffmpeg-debug.so
-  arm-linux-androideabi-strip --strip-unneeded $PREFIX/libffmpeg.so
+  $CROSS_PREFIX-strip --strip-unneeded $PREFIX/libffmpeg.so
 
 done
--- README.md
+++ README.md
@@ -12,4 +12,4 @@ http://vec.io/posts/how-to-build-ffmpeg-with-android-ndk
 0. Install git, Android ndk
 1. `$ export ANDROID_NDK=/path/to/your/android-ndk`
 2. `$ ./FFmpeg-Android.sh`
-3. libffmpeg.so will be built to `build/ffmpeg/{neon,armv7,vfp,armv6}/`
+3. libffmpeg.so will be built to `build/ffmpeg/{neon,armv7,vfp,armv6,x86}/`
